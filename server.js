7f7a7f8b454b833f5a94b93aba6e64b5d5cdce2a
Meharlochana C <meherlochana5@gmail.com>
2026-04-05 17:49:30 +0530

    fix nodemailer + correct password

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
+
+const nodemailer = require("nodemailer"); // ✅ ADD THIS
+
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // IMPORTANT
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 });
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
