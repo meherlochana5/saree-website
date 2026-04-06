f35a37151dfb1d923561b13d78541ac003f4aaa1
Meharlochana C <meherlochana5@gmail.com>
2026-04-06 15:46:33 +0530

    OTP fix added

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const nodemailer = require("nodemailer"); // ✅ ADD THIS
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // ✅ IMPORTANT
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
